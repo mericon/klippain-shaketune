ef9ce2246c727dde5179ef9b524c3e299f0825a1
mericon <graf.carsten@gmail.com>
2023-11-28 15:48:19 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -24,7 +24,7 @@ function check_download {
 
     if [ ! -d "${K_SHAKETUNE_PATH}" ]; then
         echo "[DOWNLOAD] Downloading Klippain Shake&Tune module repository..."
-        if git -C $shaketunedirname clone https://github.com/Frix-x/klippain-shaketune.git $shaketunebasename; then
+        if git -C $shaketunedirname clone https://github.com/mericon/klippain-shaketune.git $shaketunebasename; then
             chmod +x ${K_SHAKETUNE_PATH}/install.sh
             printf "[DOWNLOAD] Download complete!\n\n"
         else
